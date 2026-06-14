7b1ad217cc848b9fa054fc5d8f8b6ed66c2b69a9
nikhil <nikhilmadar666@gmail.com>
2026-06-15 02:51:19 +0530

    'first'

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-tb(ggd8x)7r$=!-#_5q-on4)+%q7dw8d02uyuki85#1=8g^)&a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://nikhilmadar.pythonanywhere.com']
 
 
 # Application definition
